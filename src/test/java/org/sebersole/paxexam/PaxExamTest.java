002d5e5ff2884be6961952604cbc11f3364e88dd
Steve Ebersole <steve@hibernate.org>
2015-05-06 14:21:21 -0500

    Using pax-exam-environment.properties

--- src/test/java/org/sebersole/paxexam/PaxExamTest.java
+++ src/test/java/org/sebersole/paxexam/PaxExamTest.java
@@ -1,6 +1,8 @@
 package org.sebersole.paxexam;
 
 import java.io.File;
+import java.io.IOException;
+import java.util.Properties;
 import javax.inject.Inject;
 
 import org.hibernate.Session;
@@ -19,20 +21,16 @@ import org.ops4j.pax.exam.Option;
 import org.ops4j.pax.exam.ProbeBuilder;
 import org.ops4j.pax.exam.TestProbeBuilder;
 import org.ops4j.pax.exam.junit.PaxExam;
-import org.ops4j.pax.exam.options.UrlReference;
 import org.ops4j.pax.exam.spi.reactors.ExamReactorStrategy;
 import org.ops4j.pax.exam.spi.reactors.PerClass;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
-import static org.ops4j.pax.exam.CoreOptions.bundle;
 import static org.ops4j.pax.exam.CoreOptions.maven;
-import static org.ops4j.pax.exam.CoreOptions.mavenBundle;
 import static org.ops4j.pax.exam.CoreOptions.options;
 import static org.ops4j.pax.exam.CoreOptions.repositories;
 import static org.ops4j.pax.exam.CoreOptions.repository;
-import static org.ops4j.pax.exam.CoreOptions.when;
 import static org.ops4j.pax.exam.karaf.options.KarafDistributionOption.debugConfiguration;
 import static org.ops4j.pax.exam.karaf.options.KarafDistributionOption.features;
 import static org.ops4j.pax.exam.karaf.options.KarafDistributionOption.karafDistributionConfiguration;
@@ -46,18 +44,15 @@ public class PaxExamTest {
 
 	@Configuration
 	public Option[] config() throws Exception {
+		Properties paxExamEnvironment = loadPaxExamEnvironmentProperties();
+
 		return options(
-				debugConfiguration( "5005", true ),
+//				debugConfiguration( "5005", true ),
 				karafDistributionConfiguration()
-						.frameworkUrl(
-								maven().groupId( "org.apache.karaf" )
-										.artifactId( "apache-karaf" )
-										.type( "tar.gz" )
-										.version( "3.0.3" )
-						)
-						.karafVersion( "3.0.3" )
+						.frameworkUrl( paxExamEnvironment.getProperty( "org.ops4j.pax.exam.container.karaf.distroUrl" ) )
+						.karafVersion( paxExamEnvironment.getProperty( "org.ops4j.pax.exam.container.karaf.version" ) )
 						.name( "Apache Karaf" )
-						.unpackDirectory( new File( "target/karaf" ) )
+						.unpackDirectory( new File( paxExamEnvironment.getProperty( "org.ops4j.pax.exam.container.karaf.unpackDir" ) ) )
 						.useDeployFolder( false ),
 				repositories(
 						repository( "https://repository.jboss.org/nexus/content/groups/public-jboss/" )
@@ -66,8 +61,13 @@ public class PaxExamTest {
 				),
 				features( featureXmlUrl(), "hibernate-native", "hibernate-jpa" ),
 				features( testingFeatureXmlUrl(), "hibernate-osgi-testing" )
-				//, mavenBundle().groupId( "com.h2database" ).artifactId( "h2" ).version( "1.3.170" ).start()
 		);
+	}
+
+	private static Properties loadPaxExamEnvironmentProperties() throws IOException {
+		Properties props = new Properties();
+		props.load( PaxExamTest.class.getResourceAsStream( "/pax-exam-environment.properties" ) );
+		return props;
 	}
 
 	private static String featureXmlUrl() {
@@ -95,21 +95,21 @@ public class PaxExamTest {
 		// and use defined imports instead
 		probe.setHeader(
 				Constants.IMPORT_PACKAGE,
-				"javassist.util.proxy" +
-						",javax.persistence" +
-						",javax.persistence.spi" +
-						",org.h2" +
-						",org.osgi.framework" +
-						",org.hibernate" +
-						",org.hibernate.boot.model" +
-						",org.hibernate.boot.registry.selector" +
-						",org.hibernate.boot.registry.selector.spi" +
-						",org.hibernate.cfg" +
-						",org.hibernate.engine.spi" +
-						",org.hibernate.integrator.spi" +
-						",org.hibernate.proxy" +
-						",org.hibernate.service" +
-						",org.hibernate.service.spi"
+				"javassist.util.proxy"
+						+ ",javax.persistence"
+						+ ",javax.persistence.spi"
+						+ ",org.h2"
+						+ ",org.osgi.framework"
+						+ ",org.hibernate"
+//						+ ",org.hibernate.boot.model"
+//						+ ",org.hibernate.boot.registry.selector"
+//						+ ",org.hibernate.boot.registry.selector.spi"
+//						+ ",org.hibernate.cfg"
+//						+ ",org.hibernate.engine.spi"
+//						+ ",org.hibernate.integrator.spi"
+//						+ ",org.hibernate.proxy"
+//						+ ",org.hibernate.service"
+//						+ ",org.hibernate.service.spi"
 //						+ ",org.ops4j.pax.exam.options"
 //						+ ",org.ops4j.pax.exam"
 		);
@@ -122,17 +122,21 @@ public class PaxExamTest {
 	@SuppressWarnings("UnusedDeclaration")
 	private BundleContext bundleContext;
 
-	@Test
-	public void test() throws Exception {
-		System.out.println( "Injected BundleContext shows the following bundles available:" );
-		for ( Bundle bundle : bundleContext.getBundles() ) {
-			System.out.println( "    > " + bundle.getSymbolicName() + ":" + bundle.getVersion() );
-		}
-	}
+//	@Test
+//	public void test() throws Exception {
+//		System.out.println( "Injected BundleContext shows the following bundles available:" );
+//		for ( Bundle bundle : bundleContext.getBundles() ) {
+//			System.out.println( "    > " + bundle.getSymbolicName() + ":" + bundle.getVersion() );
+//		}
+//	}
 
 	@Test
 	public void testNative() throws Exception {
+		// This line causes our OsgiSessionFactoryService to execute, and it *seems* to build properly.
+		//		I say "seems" because the debugger does some strange things and I cannot step into
+		//		everything.
 		final ServiceReference sr = bundleContext.getServiceReference( SessionFactory.class.getName() );
+		// This line for sure returns null, leading to the NPE on the line following this.
 		final SessionFactory sf = (SessionFactory) bundleContext.getService( sr );
 
 		Session s = sf.openSession();
